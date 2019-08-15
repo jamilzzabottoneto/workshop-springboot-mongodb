33b9154d0e28530f2819daf6a22d3977c36dac79
jamil Zabotto Neto <jamilzabneto@hotmail.com>
2019-08-15 16:01:48 -0300

    Conectando ao MongoDB com REpository e Service

---
+++ src/main/java/com/jamilneto/workshopmongo/service/UserService.java
@@ -0,0 +1,20 @@
+package com.jamilneto.workshopmongo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jamilneto.workshopmongo.domain.User;
+import com.jamilneto.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll(){
+		return repo.findAll();
+	}
+}
