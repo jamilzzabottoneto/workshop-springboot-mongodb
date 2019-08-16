e958309c283a8e3bcb65fa4836446ee93690d228
jamil Zabotto Neto <jamilzabneto@hotmail.com>
2019-08-16 14:36:22 -0300

    Consulta com varios critérios

--- src/main/java/com/jamilneto/workshopmongo/service/PostService.java
+++ src/main/java/com/jamilneto/workshopmongo/service/PostService.java
@@ -1,5 +1,6 @@
 package com.jamilneto.workshopmongo.service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,17 +13,23 @@ import com.jamilneto.workshopmongo.service.exception.ObjectNotFoundException;
 
 @Service
 public class PostService {
-	
+
 	@Autowired
 	private PostRepository repo;
-	
+
 	public Post findById(String id) {
 		Optional<Post> user = repo.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto nao Encontrado"));
 	}
-	
-	public List<Post> findByTitle(String text){
+
+	public List<Post> findByTitle(String text) {
 		return repo.searchTitle(text);
+	}
+
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+
+		maxDate = new Date(maxDate.getTime() + 4 * 60 * 60 * 1000);
+		return repo.fullsearch(text, minDate, maxDate);
 	}
 
 }
