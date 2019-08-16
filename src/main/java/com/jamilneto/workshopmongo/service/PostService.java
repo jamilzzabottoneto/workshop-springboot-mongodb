108f16b05cc3e5b4bc96a1710df99f6b01ff030d
jamil Zabotto Neto <jamilzabneto@hotmail.com>
2019-08-16 12:52:28 -0300

    Obtendo um post por ID

---
+++ src/main/java/com/jamilneto/workshopmongo/service/PostService.java
@@ -0,0 +1,23 @@
+package com.jamilneto.workshopmongo.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jamilneto.workshopmongo.domain.Post;
+import com.jamilneto.workshopmongo.repository.PostRepository;
+import com.jamilneto.workshopmongo.service.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> user = repo.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto nao Encontrado"));
+	}
+
+}
